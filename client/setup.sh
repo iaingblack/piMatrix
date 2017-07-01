77602471193f0d30cd35b763501023a7c869a954
Iain Black <iaingblack@gmail.com>
2017-07-01 10:27:38 +0100

    Added missing setup.sh stuff for client

--- client/setup.sh
+++ client/setup.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+clear
 apt-get update
 apt-get upgrade -y
+sudo wget -q https://raw.githubusercontent.com/iaingblack/piMatrix/master/client/client-send.py -O client-send.py
+
+echo TO RUN TYPE SOMETHING LIKE THIS - python client-send.py myserver myport 160 120
