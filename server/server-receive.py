4498bb78b4d1e282fa1f0c0ea6a96041e709fda4
Iain Black <iaingblack@gmail.com>
2017-07-01 10:23:50 +0100

    Think it works. Re-test on fresh machine then tidy

--- server/server-receive.py
+++ server/server-receive.py
@@ -6,40 +6,12 @@ from PIL import Image
 #############################################################################
 # CODE TO CONVERT TO ASCII
 #############################################################################
-ASCII_CHARS = [
-    " ",
-    ".",
-    "'",
-    "-",
-    ":",
-    ";",
-    "!",
-    "~",
-    "*",
-    "+",
-    "?",
-    "r",
-    "t",
-    "e",
-    "x",
-    "z",
-    "m",
-    "6",
-    "8",
-    "g",
-    "%",
-    "#",
-    "W",
-    "M",
-    "B",
-    "@",
-]
+ASCII_CHARS = [" ",".","'","-",":",";","!","~","*","+","?","r","t","e","x","z","m","6","8","g","%","#","W","M","B","@",]
 
 def scale_image(image, new_width=180):
-    """Resizes an image preserving the aspect ratio.
-    """
     (original_width, original_height) = image.size
     aspect_ratio = original_height/float(original_width)
+    #To counter general issue that ascii characters are tall and not square
     new_height = int(aspect_ratio * new_width)*6/11
     new_image = image.resize((new_width, new_height))
     return new_image
@@ -48,12 +20,6 @@ def convert_to_grayscale(image):
     return image.convert('L')
 
 def map_pixels_to_ascii_chars(image, range_width=11):
-    """Maps each pixel to an ascii char based on the range
-    in which it lies.
-
-    0-255 is divided into 11 ranges of 25 pixels each.
-    """
-
     pixels_in_image = list(image.getdata())
     pixels_to_chars = [ASCII_CHARS[pixel_value/range_width] for pixel_value in
             pixels_in_image]
@@ -61,7 +27,10 @@ def map_pixels_to_ascii_chars(image, range_width=11):
     return "".join(pixels_to_chars)
 
 def convert_image_to_ascii(image, new_width=180):
-    image = scale_image(image)
+    #Use size of image a width, what we pass from camera is what we want in return
+    new_width=image.size[0]
+    #Reference returned image new size as it could be updated along hte way
+    image = scale_image(image, image.size[0])
     image = convert_to_grayscale(image)
 
     pixels_to_chars = map_pixels_to_ascii_chars(image)
@@ -71,9 +40,6 @@ def convert_image_to_ascii(image, new_width=180):
             xrange(0, len_pixels_to_chars, new_width)]
 
     return "\n".join(image_ascii)
-
-def handle_image_conversion(image_filepath):
-    image_ascii = convert_image_to_ascii(image)
 
 #############################################################################
 # CODE TO RECEIVE IMAGE
@@ -94,23 +60,19 @@ def receive_video_stream(port):
             image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
             if not image_len:
                 break
-            # Construct a stream to hold the image data and read the image
-            # data from the connection
+            # Construct a stream to hold the image data and read the data from the connection
             image_stream = io.BytesIO()
             image_stream.write(connection.read(image_len))
-            # Rewind the stream, open it as an image with PIL and do some
-            # processing on it
+            # Rewind the stream, open as PIL image and do process it
             image_stream.seek(0)
             image = Image.open(image_stream)
             print('Image is %dx%d' % image.size)
+            print(str(image.size[0])+'x'+str(image.size[1]))
             ##### SAVE TO ASCII
             ascii = convert_image_to_ascii(image)
             text_file = open("/var/www/html/ascii-text.txt", "w")
             text_file.write(ascii)
             text_file.close()
-            #imagetoconverttoascii.save("/usr/imagetoconverttoascii.jpg")
-            #print('Image is saved as imagetoconverttoascii.jpg')
-        #image.save('imagetoconvert.jpg')
     finally:
         connection.close()
         server_socket.close()
